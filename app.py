45570fe346a3fa7427eb46c60d8d13c8a4941606
Tiisetsopudinyane <pudinyanesamueltiisetso@gmail.com>
2024-11-20 12:36:18 +0200

    app.py modified

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
 
 from accounts import create_app
-
+from waitress import serve
 config_type = os.getenv("FLASK_ENV")
 
 app = create_app(config_type)
 
 if __name__ == "__main__":
-    app.run()
+    serve(app, host='0.0.0.0', port=8000)
