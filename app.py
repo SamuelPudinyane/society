a24d71790798bce2487958bf8a3e31850fa119e9
Tiisetsopudinyane <pudinyanesamueltiisetso@gmail.com>
2025-10-22 20:24:17 +0200

    chnaged connection string

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-
+from dotenv import load_dotenv
 from accounts import create_app
 from waitress import serve
 config_type = os.getenv("FLASK_ENV","development")
@@ -7,4 +7,6 @@ config_type = os.getenv("FLASK_ENV","development")
 app = create_app(config_type)
 app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024 * 1024
 if __name__ == "__main__":
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     serve(app, host='0.0.0.0', port=5000)
